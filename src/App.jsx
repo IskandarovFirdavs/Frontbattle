28baa871d725f1ddc15bbaf537b3295309b13d93
IskandarovFirdavs <145989337+IskandarovFirdavs@users.noreply.github.com>
2024-08-25 11:02:42 +0500

    commit bir

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        {/* <Route path="/" element={}></Route>
+        <Route path="/" element={}></Route>
+        <Route path="/" element={}></Route>
+        <Route path="/" element={}></Route>
+        <Route path="/" element={}></Route> */}
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
